b69011ce0b87662b4d74e5b0ff684cb0ed0f5563
Tiago Samaha <tiagosamaha@gmail.com>
2014-08-05 16:36:13 -0300

    hashie mailer

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,7 +1,7 @@
 config_file = YAML.load_file(Rails.root.join('config', 'mail.yml'))
 #config_file = File.join(::Rails.root, 'config', 'mail.yml')
 #mail = File.exists?(config_file) ? YAML.load(File.read(config_file))[Rails.env] : {}
-mail = File.exists?(config_file) ? Hashie::Mash.new(settings_hash[Rails.env]) : {}
+mail = File.exists?(config_file) ? Hashie::Mash.new(config_file[Rails.env]) : {}
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.delivery_method = :test if Rails.env.test? || Rails.env.development?
